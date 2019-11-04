159f95a5a233098124092f451fc7a448230308d3
Robbie Singer <robbiebsinger@gmail.com>
2019-11-04 15:01:22 +0000

    Update task list

--- tasks.py
+++ tasks.py
@@ -1,13 +1,25 @@
 from invoke import task
 
 @task
+def install_dependencies(c):
+    c.run("pip install -r requirements.txt")
+
+@task
+def check_python_formatting(c):
+    c.run("black --check python/*.py")
+
+@task
 def build_docker(c):
-    c.run("docker build -t iati-partner-search .")
+    c.run("docker build -t rabshab/iati-partner-search-app -f .\app.Dockerfile .")
 
 @task
 def run_docker(c):
-    c.run("docker run --name=ips -it -v ${pwd}:/iati-partner-search -p 5000:5000 iati-partner-search bash")
+    c.run("docker run --name=ipsapp -p 5000:5000 rabshab/iati-partner-search-app")
 
 @task
-def start(c):
-    c.run("python -m flask run --host=0.0.0.0")
+def push_docker(c):
+    c.run("docker push rabshab/iati-partner-search-app:latest")
+
+@task(install_dependencies, check_python_formatting, build_docker, push_docker)
+def ci(c):
+    print("Running CI scripts")
