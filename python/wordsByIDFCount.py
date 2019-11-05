a751a946fff95139f7386ed6c427408dacc58064
nathanbegbie <nathanbegbie@gmail.com>
2019-11-05 10:32:38 +0000

    blacken all python code in repository

--- python/wordsByIDFCount.py
+++ python/wordsByIDFCount.py
@@ -10,31 +10,30 @@ import numpy as np
 import os
 import pickle
 
-wd = r'C:\Users\t-wilson\Documents\IATI_partner_search\download_stemEngDict'
+wd = r"C:\Users\t-wilson\Documents\IATI_partner_search\download_stemEngDict"
 
 
-#Import Pickle file of doc-term matrix
-with open(os.path.join(wd,'iatiFullTDMstemEngDict.pkl'), "rb") as f:
+# Import Pickle file of doc-term matrix
+with open(os.path.join(wd, "iatiFullTDMstemEngDict.pkl"), "rb") as f:
     X = pickle.load(f)
 
-#Import Pickle file of IDF
-with open(os.path.join(wd, 'iatiTDM_IDFstemEngDict.pkl'), 'rb') as f:
+# Import Pickle file of IDF
+with open(os.path.join(wd, "iatiTDM_IDFstemEngDict.pkl"), "rb") as f:
     idf = pickle.load(f)
-    
-#Import Pickle file of word list
-with open(os.path.join(wd, 'wordListstemEngDict.pkl'), 'rb') as f:
+
+# Import Pickle file of word list
+with open(os.path.join(wd, "wordListstemEngDict.pkl"), "rb") as f:
     words = pickle.load(f)
-    
-wordsDF = pd.DataFrame(words, columns=['word'])
 
-#Add average idf to word list
-wordsDF['idf'] = idf
+wordsDF = pd.DataFrame(words, columns=["word"])
 
-#Get the words greater than 0
+# Add average idf to word list
+wordsDF["idf"] = idf
+
+# Get the words greater than 0
 nonZeroCount = X.getnnz(axis=0)
 
-wordsDF['count'] = nonZeroCount
+wordsDF["count"] = nonZeroCount
 
-#write to csv
-wordsDF.to_csv(os.path.join(wd, 'StemmingWordsFreqIDF.csv'), index = False)
-
+# write to csv
+wordsDF.to_csv(os.path.join(wd, "StemmingWordsFreqIDF.csv"), index=False)
