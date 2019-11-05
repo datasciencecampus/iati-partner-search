42ebbc170b25df493404c1e2cab7131539181ee4
nathanbegbie <nathanbegbie@gmail.com>
2019-11-05 10:32:38 +0000

    blacken all python code in repository

--- python/utils.py
+++ python/utils.py
@@ -1,11 +1,13 @@
 from os.path import join, dirname, abspath
 from datetime import datetime
 
+
 def get_timestamp_string_prefix():
     """
     return the date and time as a string in the format 2019_10_14_18_14_11
     """
     return datetime.now().strftime("%G_%m_%d_%H_%M_%S")
+
 
 def get_data_path():
     """
@@ -14,6 +16,7 @@ def get_data_path():
     Should work consistently across OS
     """
     return join(dirname(dirname(abspath(__file__))), "data")
+
 
 def get_input_path():
     """
