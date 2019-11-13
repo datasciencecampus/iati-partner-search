6ddc910b8f70fd33fc37717da2d799c4a085a43e
Tom Wilson <t-wilson@dfid.gov.uk>
2019-11-13 17:19:06 +0000

    Append to list to see if faster, incremental progress

--- python/word2vecaverage.py
+++ python/word2vecaverage.py
@@ -22,15 +22,16 @@ def average_per_doc(description_text, w2v_model, dim_size=300):
 
 
 def results_per_corpus_df(input_df, w2v_model, avg_filename, dim_size=300):
-    results_arr = np.empty([0, dim_size])
+    results_arr = []
+    progress = set([i for i in range(100000, 1000000, 100000)])
     for index, row in input_df.iterrows():
-        results_arr = np.vstack(
-            (results_arr, average_per_doc(row["description"], w2v_model, dim_size))
-        )
+        results_arr.append(average_per_doc(row["description"], w2v_model, dim_size))
+        if index in progress:
+            print("processed {0} records".format(index))
+    results_arr = np.array(results_arr)    
     with open(join(get_data_path(), avg_filename), "wb") as output_file:
         pickle.dump(results_arr, output_file)
     
-
 
 if __name__ == "__main__":
     
@@ -40,10 +41,9 @@ if __name__ == "__main__":
     
     df1 = df1[['description']]
 
-
     model = Word2Vec.load(join(get_data_path(), WORD2VECMODEL_FILENAME))
 
     # This takes a while
     start = time.time()
     results_per_corpus_df(df1, model, WORD2VECAVG_FILENAME, 50)
-    print("average array created in {0}".format(time.time()- start))
+    print("average array created in {0} seconds".format(time.time()- start))
