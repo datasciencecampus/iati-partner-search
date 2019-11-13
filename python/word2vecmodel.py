cd2a6b58ea6ef35ffaad2b03db4f4cd5ab6c32c0
Tom Wilson <t-wilson@dfid.gov.uk>
2019-11-13 11:43:29 +0000

    Added timings to w2v model write-out

--- python/word2vecmodel.py
+++ python/word2vecmodel.py
@@ -2,6 +2,7 @@ from utils import get_data_path
 import pandas as pd
 from os.path import join
 from gensim.models import Word2Vec
+import time
 
 from constants import PROCESSED_RECORDS_FILENAME
 
@@ -11,12 +12,15 @@ def build_w2v_model(input_df, dim_size):
 
 
 if __name__ == "__main__":
-
+    
 
     df1 = pd.read_csv(
         join(get_data_path(), PROCESSED_RECORDS_FILENAME), encoding="iso-8859-1"
     )
-
+    
+    start = time.time()
     model = build_w2v_model(df1, 50)
     
     model.save(join(get_data_path(), "word2vec.model"))
+    
+    print("saved w2v model in {0} seconds".format(time.time() - start))
