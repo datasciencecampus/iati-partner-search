45273e1b89789d8e1d7f241dd4ff127ebc88f831
nathanbegbie <n-begbie@dfid.gov.uk>
2019-12-04 11:30:34 +0000

    fix conf.py import ordering

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+import datetime
+from recommonmark.parser import CommonMarkParser
+
+sys.path.insert(0, os.path.abspath(".."))
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -10,14 +17,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
-import datetime
-from recommonmark.parser import CommonMarkParser
+
 
 project = "IATI Partner Search"
 copyright = f"{datetime.datetime.now().year}, Department for International Development"
